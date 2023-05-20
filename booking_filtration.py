f6655ae8970cf825e0e1a7a2057ccae5c2464ea6
Serzh778 <134130137+Serzh778@users.noreply.github.com>
2023-05-20 14:50:48 +0700

    Update booking_filtration.py

--- booking_filtration.py
+++ booking_filtration.py
@@ -6,11 +6,13 @@ from booking.booking_filtration import BookingFiltration
 from selenium.webdriver.remote.webdriver import WebDriver
 
 class BookingFiltration():
-    def __init__(self, driver:WebDriver):
-       
-       self.driver = driver
+    def __init__(self, driver: WebDriver):
+        # The `BookingFiltration` class handles the filtration functionality on the booking.com website.
+        # It takes a WebDriver object as a parameter to interact with the browser.
+        self.driver = driver
 
     def star_filtration(self):
+        # Applies the star filtration to filter the results for five-star hotels.
         column = self.driver.find_element(By.ID, 'filter_group_class_:R14q:')
         five_star = column.find_element(By.ID, ':Rlf94q:')
         five_star.click()
